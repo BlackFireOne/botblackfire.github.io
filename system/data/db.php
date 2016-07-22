0c1bb62399a3e7c86d451b865b92964dabe5bc82
BlackFireOne <andrianov.02@inbox.ru>
2016-07-22 06:58:34 +0400

    Initial commit

---
+++ system/data/db.php
@@ -0,0 +1,17 @@
+<?php
+
+define ("DBHOST", "mysql.nazuka.net"); 
+
+define ("DBNAME", "u854468503_dd");
+
+define ("DBUSER", "u854468503_dd");
+
+define ("DBPASS", "xxx99911122");  
+
+define ("PREFIX", "vii"); 
+
+define ("COLLATE", "utf8");
+
+$db = new db;
+
+?>
